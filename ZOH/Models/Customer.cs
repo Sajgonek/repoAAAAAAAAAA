5d5a0bcd71270551fe681caa8739896f9eb0c2bf
Sajgonek <34008096+Sajgonek@users.noreply.github.com>
2017-12-11 15:27:50 +0100

    Before Migrations

---
+++ ZOH/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZOH.Models
+{
+    public class Customer
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public int TelNumber { get; set; }
+        public Room Room { get; set; }
+        public byte RoomId { get; set; }
+    }
+}
---
+++ ZOH/Models/Room.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZOH.Models
+{
+    public class Room
+    {
+        public byte Id { get; set; }
+        public string RoomType { get; set; }
+    }
+}
